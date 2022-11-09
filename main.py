71584907025cc57fc78954105bf106e59ad4057f
poonamgaud <114494533+poonamgaud@users.noreply.github.com>
2022-11-09 09:46:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import time
 import plotly.express as px
 
-# read csv from a github repo
+
 df = pd.read_csv("Bank.csv")
 
 st.set_page_config(
@@ -13,30 +13,25 @@ st.set_page_config(
  layout='wide'
 )
 
-# dashboard title
 
 st.title("Real-Time / Live Data Science Dashboard")
 
-# top-level filters
+
 
 job_filter = st.selectbox("Select the Job", pd.unique(df['job']))
-
-# creating a single-element container.
 placeholder = st.empty()
 
-# dataframe filter
+
 
 df = df[df['job'] == job_filter]
 
-# near real-time / live feed simulation
+
 
 for seconds in range(200):
- # while True:
+ 
 
  df['age_new'] = df['age'] * np.random.choice(range(1, 5))
  df['balance_new'] = df['balance'] * np.random.choice(range(1, 5))
-
- # creating KPIs
  avg_age = np.mean(df['age_new'])
 
  count_married = int(df[(df["marital"] == 'married')]['marital'].count() + np.random.choice(range(1, 30)))
@@ -46,13 +41,11 @@ for seconds in range(200):
  with placeholder.container():
      kpi1, kpi2, kpi3 = st.columns(3)
 
- # fill in those three columns with respective metrics or KPIs
  kpi1.metric(label="Age ⏳", value=round(avg_age), delta=round(avg_age) - 10)
  kpi2.metric(label="Married Count 💍", value=int(count_married), delta=- 10 + count_married)
  kpi3.metric(label="A/C Balance ＄", value=f"$ {round(balance, 2)} ",
  delta=- round(balance / count_married) * 100)
 
- # create two columns for charts
 
  fig_col1, fig_col2 = st.columns(2)
  with fig_col1:
